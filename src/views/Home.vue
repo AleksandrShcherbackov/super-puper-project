1692577ef4ef16557f2521642bf464b9edac2b61
Щербаков Александр Владимирович <sherbakov@arcsinus.ru>
2025-04-07 20:11:56 +0300

    :lipstick: Home.vue style fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,19 +10,22 @@
   </script>
   
   <template>
-    <div class="bg-cover bg-center h-screen w-full flex flex-col justify-between" style="background-image: url('/background.jpg')">
+    <div class=" h-screen w-full flex flex-col" >
       <Header />
-      <section class="container text-white p-8">
-        <h1>{{ $t('home.title') }}</h1>
-        <p>{{ $t('home.description1') }}</p>
-        <p>{{ $t('home.description2') }}</p>
-        <router-link to="/order" class="bg-blue-500 text-white px-4 py-2 rounded">{{ $t('home.cta') }}</router-link>
+      <section class="bg-darkBlue text-white pt-8 pb-8" >
+        <div class="container mx-auto">
+          <h1>{{ $t('home.title') }}</h1>
+          <p>{{ $t('home.description1') }}</p>
+          <p>{{ $t('home.description2') }}</p>
+          <router-link to="/order" class="bg-orange text-white px-4 py-2 rounded">{{ $t('home.cta') }}</router-link>
+        </div>
+        
       </section>
   
       <!-- Pricing Section -->
-      <section class="container flex justify-around mt-8">
+      <section class="bg-cover bg-center text-white p-8 h-full max-h-12" style="background-image: url('./src/assets/images/main-bg-1.jpg')">
         <!-- Пример карточек тарифов -->
-        
+        <div class="container mx-auto"></div>
       </section>
   
       <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,ts,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        darkBlue: {
+          DEFAULT: '#606E8C', // Основной темно-синий цвет
+          light: '#A2A2D0',    // Светлый оттенок темно-синего
+          dark: '#B0B7C6',     // Темный оттенок темно-синего
+        },
+        orange: {
+          DEFAULT: '#F97316',  // Основной оранжевый цвет
+          light: '#FB923C',    // Светлый оттенок оранжевого
+          dark: '#C2410C',     // Темный оттенок оранжевого
+        },
+      },
+    },
   },
   plugins: []
 }

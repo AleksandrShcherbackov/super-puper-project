bb3dd736e771f77fcafca3c2490bbec47d111067
Щербаков Александр Владимирович <sherbakov@arcsinus.ru>
2025-04-09 19:06:23 +0300

    :sparkles: added logic for dinamic localization in url

--- src/components/base/BaseHeader.vue
+++ src/components/base/BaseHeader.vue
@@ -1,4 +1,10 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const prefix = computed(() => route.path.includes('en') ? 'en' : 'ru')
+</script>
 
 <template>
   <header class="bg-gray-800 text-white p-4">
@@ -7,9 +13,9 @@
         <router-link to="/" class="flex items-center justify-center mr-4">
           <img src="../../assets/icons/notebook.svg" alt="logo" class="w-10 h-10" />
         </router-link>
-        <router-link to="/en" class="mr-4">{{ $t('header.home') }}</router-link>
-        <router-link to="/en/tos">{{ $t('header.terms') }}</router-link>
-        <router-link to="/en/order">{{ $t('header.order') }}</router-link>
+        <router-link :to="`/${prefix}`" class="mr-4">{{ $t('header.home') }}</router-link>
+        <router-link :to="`/${prefix}/tos`">{{ $t('header.terms') }}</router-link>
+        <router-link :to="`/${prefix}/order`">{{ $t('header.order') }}</router-link>
       </nav>
     </div>
   </header>
